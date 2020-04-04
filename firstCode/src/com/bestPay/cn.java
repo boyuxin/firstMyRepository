2ca4e90292ca3fde6711fe7027edf092739c4dfb
boyuxin <1969591029@qq.com>
2020-04-04 23:32:48 +0800

    Hello Git

---
+++ firstCode/src/com/bestPay/cn.java
@@ -0,0 +1,12 @@
+package com.bestPay;
+
+/**
+ * @author boyuxin
+ * @description
+ * @date 2020/4/4 23:31
+ */
+public class cn {
+    public static void main(String[] args) {
+        System.out.println("Hello git");
+    }
+}
